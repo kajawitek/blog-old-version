07ab2235c187cc96d0db800fbdfb2714a9593bdd
kajawitek <kajawasiolek@gmail.com>
2022-09-22 17:02:24 +0200

    feat: setup author account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+puts '# Creating Authors'
+3.times do |a|
+  author = Author.where(email: "author_#{a + 1}@example.com").first_or_create!(password: 'password')
+  puts author.email
+end
