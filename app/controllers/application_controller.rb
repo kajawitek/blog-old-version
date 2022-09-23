a21f97acc4a3f6b05f2f35b533cb7aa320dde874
kajawitek <kajawasiolek@gmail.com>
2022-09-23 19:59:40 +0200

    fix: make changes in application.js and application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_author!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_author!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
