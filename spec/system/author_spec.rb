37bfc53b56d904d7ea5cca48eba61d5f6854ffb0
kajawitek <kajawasiolek@gmail.com>
2022-10-13 12:29:16 +0200

    test: add missing account settings test to author_spec

--- spec/system/author_spec.rb
+++ spec/system/author_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 describe 'Author spec' do
   let!(:author) { build(:author) }
 
-  it 'registers and logs in and logs out' do
+  it 'registers and logs in and logs out and changes account settings' do
     visit root_path
+
     click_link 'Register'
     fill_in 'author[first_name]', with: author.first_name
     fill_in 'author[last_name]', with: author.last_name
@@ -15,12 +16,22 @@ describe 'Author spec' do
     expect(page).to have_content 'You have signed up successfully.'
 
     click_button 'Sign Out'
-
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
+
     fill_in 'author[email]', with: author.email
     fill_in 'author[password]', with: author.password
     click_button 'Log in'
-
     expect(page).to have_button 'Sign Out'
+
+    click_link 'Account settings'
+    fill_in 'author[first_name]', with: 'New author first name'
+    fill_in 'author[last_name]', with: 'New author last name'
+    fill_in 'author[email]', with: 'new_email@test.com'
+    fill_in 'author[password]', with: 'new_password'
+    fill_in 'author[password_confirmation]', with: 'new_password'
+    fill_in 'author[current_password]', with: author.password
+    click_button 'Update'
+
+    expect(page).to have_content 'Your account has been updated successfully.'
   end
 end
